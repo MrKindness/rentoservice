6c4bcf90df1a046603b28a52315dc082d87725be
trickster5 <trickster.vastem@gmail.com>
2024-09-08 15:51:47 +0300

    first commit

---
+++ src/main/java/com/rento/service/rentoservice/RentoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rento.service.rentoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rento/service/rentoservice/RentoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rento.service.rentoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
